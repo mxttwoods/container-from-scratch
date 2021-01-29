63f489710b45e75ca3b61fe7923db6ba6cc0841b
Matthew Woods <mxttwoods@gmail.com>
2021-01-28 23:41:53 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# container-from-scratch
- Inspired by Liz Rice's from scratch series
+# [container-from-scratch](https://www.youtube.com/watch?v=8fi7uSYlOdc)
+
+Inspired by Liz Rice's from scratch series
+
+## [Liz Rice Demo](https://github.com/lizrice/containers-from-scratch)
+
+Writing a container in a few lines of Go code, as seen at DockerCon 2017 and on O'Reilly Safari
+
+You need root permissions for this version to work. Or you can adapt it to be a rootless container by as shown in these slides.
+
+Note that the Go code uses some syscall definitions that are only available when building with GOOS=linux.
--- src/main.go
+++ src/main.go
@@ -4,29 +4,54 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
-func main(){
+// main cli controller
+func main() {
 	switch os.Args[1] {
 	case "run":
-			run()
+		run()
+	case "child":
+		child()
 	default:
-			panic("arg not recognized")
+		panic("arg not recognized")
 	}
 }
 
-func run(){
-	fmt.Printf("running %v\n", os.Args[2:])
+// main container runtime
+func run() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2], os.Args[3:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+	// run
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd
-
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+	// create root
+	must(syscall.Chroot("home/matthew"))
+	// cd into root
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+	// run
 	must(cmd.Run())
 }
 
-func must(err error){
+// catch errors
+func must(err error) {
 	if err != nil {
 		panic(err)
 	}
